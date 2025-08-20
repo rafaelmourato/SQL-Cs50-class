245f810741a8fa718ba171d32c73f08484c32486
Rafael <rafael.mourato@hotmail.com>
2025-08-19 23:51:54 -0300

    Close to ending the class

--- flights/views.py
+++ flights/views.py
@@ -7,3 +7,11 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request,flight_id):
+    flight = Flight.objects.get(pk=  flight_id ) #pk = primary key
+    #adicionar verificação
+    return render(request, "flights/flight.html",{
+        "flight": flight,
+        "passengers": flight.passengers.all()
+        })
